c8d74d16c13292fcd6f30b7e0587d4d2f06e8936
Slawek <61154596+Ultran@users.noreply.github.com>
2021-10-11 12:52:54 +0200

    project with redux completed, next to do redux saga

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import CounterContainer from "./counter/CounterContainer";
 import Posts from "./posts/containers/Posts";
 import rootReducer from "./RootReducer";
 import "./styles.css";
+import Appbar from "./ui/containers/Appbar";
 import Users from "./users/containers/Users";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["ui", "counter"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = [thunk];
+
+const store = createStore(
+  persistedReducer,
+  /* preloadedState, */
+  composeEnhancers(applyMiddleware(...middleware))
+);
+
+const persistor = persistStore(store);
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Provider store={store}>
-          <CounterContainer />
-          <Posts />
-          <Users />
+          <PersistGate loading={null} persistor={persistor}>
+            <Appbar />
+            <CounterContainer />
+            <Posts />
+            <Users />
+          </PersistGate>
         </Provider>
       </div>
     );
--- src/RootReducer.js
+++ src/RootReducer.js
@@ -1,12 +1,15 @@
 import { combineReducers } from "redux";
+
 import counterReducer from "./counter/redux";
 import postsReducer from "./posts/redux";
 import usersReducer from "./users/redux";
+import uiReducer from "./ui/redux";
 
 const rootReducer = combineReducers({
   counter: counterReducer,
   posts: postsReducer,
   users: usersReducer,
+  ui: uiReducer,
 });
 
 export default rootReducer;
